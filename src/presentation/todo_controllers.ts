d3300e08b0ed1d4ec771cd4a2ca0cb3b671b9ddd
Marcos Adriano <marcosadriano740@gmail.com>
2022-04-19 22:27:38 -0300

    feat: create todo controller returns status 201 on success

--- src/presentation/todo_controllers.ts
+++ src/presentation/todo_controllers.ts
@@ -3,29 +3,39 @@ import { Controller, HttpRequest, HttpResponse } from "./controller.ts";
 
 export class CreateTodoController implements Controller {
   handle(request: HttpRequest): Promise<HttpResponse> {
-    if (!request.body.title) {
+    try {
+      if (!request.body.title) {
+        return new Promise((resolve) =>
+          resolve({
+            statusCode: 400,
+            body: { message: "Title property is necessary." },
+          })
+        );
+      }
+      const todo = new Todo(request.body.title);
+      todo.setDescription(request.body.description);
+      todo.setStartDate(request.body.startDate);
       return new Promise((resolve) =>
         resolve({
-          statusCode: 400,
-          body: { message: "Title property is necessary." },
+          statusCode: 201,
+          headers: [
+            {
+              name: "Location",
+              value: `http://localhost:5000/todo/${todo.getId()}`,
+            },
+          ],
+          body: {
+            todoId: todo.getId(),
+          },
+        })
+      );
+    } catch (error) {
+      return new Promise((resolve) =>
+        resolve({
+          statusCode: 500,
+          body: { message: error.message },
         })
       );
     }
-    const todo = new Todo(request.body.title);
-    todo.setDescription(request.body.description);
-    return new Promise((resolve) =>
-      resolve({
-        statusCode: 201,
-        headers: [
-          {
-            name: "Location",
-            value: `http://localhost:5000/todo/${todo.getId()}`,
-          },
-        ],
-        body: {
-          todoId: todo.getId(),
-        },
-      })
-    );
   }
 }
--- src/presentation/todo_controllers_test.ts
+++ src/presentation/todo_controllers_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals, assertExists } from "../../external/tests.ts";
+import { assert, assertEquals, assertExists } from "../../external/tests.ts";
 
 import { CreateTodoController } from "./todo_controllers.ts";
 
@@ -19,4 +19,38 @@ Deno.test("CreateTodoController: should return status 400 if no title is provide
   assertEquals(response.statusCode, 400);
   assertExists(response.body["message"]);
   assertEquals(response.body.message, "Title property is necessary.");
+});
+
+Deno.test("CreateTodoController: should return status 500 if an error occurs", async function () {
+  beforeEach();
+
+  const response = await createTodoController.handle({
+    body: {
+      title: "Any title",
+      description: "Any description",
+      startDate: new Date(Date.now() - 1000),
+    },
+  });
+
+  assertEquals(response.statusCode, 500);
+  assertExists(response.body.message);
+});
+
+Deno.test("CreateTodoController: should return status 201 on success", async function () {
+  beforeEach();
+
+  const response = await createTodoController.handle({
+    body: {
+      title: "Any title",
+    },
+  });
+
+  assertEquals(response.statusCode, 201);
+  assertExists(response.headers);
+  assertExists(response.body.todoId);
+  assert(
+    response.headers.find((header) =>
+      header.name === "Location" && header.value
+    ),
+  );
 });
