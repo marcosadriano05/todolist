e56c605a01932053211e85772997cf08a931694f
Marcos Adriano <marcosadriano740@gmail.com>
2022-05-24 17:47:15 -0300

    chore: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
-export * from "https://deno.land/std@0.137.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.140.0/testing/asserts.ts";
 
-export * from "https://deno.land/std@0.137.0/testing/bdd.ts";
+export * from "https://deno.land/std@0.140.0/testing/bdd.ts";
 
-export * from "https://deno.land/std@0.137.0/testing/mock.ts";
+export * from "https://deno.land/std@0.140.0/testing/mock.ts";
 
-export * from "https://deno.land/std@0.137.0/uuid/mod.ts";
+export * from "https://deno.land/std@0.140.0/uuid/mod.ts";
 
 export * from "https://deno.land/x/nessie@2.0.5/mod.ts";
 
@@ -18,4 +18,4 @@ export type {
   ParamsDictionary,
 } from "https://deno.land/x/opine@2.2.0/mod.ts";
 
-export { DB } from "https://deno.land/x/sqlite@v3.3.0/mod.ts";
+export { DB } from "https://deno.land/x/sqlite@v3.4.0/mod.ts";
